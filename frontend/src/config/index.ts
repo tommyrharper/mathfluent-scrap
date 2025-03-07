37f299a417f5c2499a004eee4f889b709eb0324a
12v <11772383+12v@users.noreply.github.com>
2025-03-07 16:46:04 +0000

    chore: update docker and configuration for deployment

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,2 +1,1 @@
-// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://10.236.239.133:8000';
-export const API_BASE_URL = 'http://10.236.239.133:8000';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8001';
